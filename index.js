930f02f98b101787f00856ae671429e46494d6eb
alshakibeelahi <alshakibeelahi@gmail.com>
2023-11-08 09:08:45 +0600

    i18Next R&D

--- index.js
+++ index.js
@@ -5,9 +5,30 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//setting up i18next
+const i18next = require('i18next');
+const i18nextMiddleware = require('i18next-http-middleware');
+const Backend = require('i18next-node-fs-backend');
+
+i18next
+.use(Backend)
+.use(i18nextMiddleware.LanguageDetector)
+.init({
+  backend: {
+    loadPath: __dirname + '/locales/{{lng}}/translation.json',
+  },
+  detection: {
+    order: ['header'],
+    caches: ['cookie']
+  },
+  preload: ['en', 'fr'],
+  fallbackLng: 'fr',
+});
+app.use(i18nextMiddleware.handle(i18next));
+
 const masterKey = 'i4y97T3G-9Hpt-jGIZ-3XqC-eFjwMSgwYun8';
-const privateToken = 'test_private_uPpwmvA2DQbKoaEeVSLrKt85wC6';
-const paydunyaToken = 'ozRX1bxEQs7oFfjDidvt';
+const privateToken = 'live_private_usA0HXYhGXCQEOp1JJcAw7qOwoM';
+const paydunyaToken = 'ZouqPb1pKM85ni1WhJF2';
 
 app.get('/disburse-', (req, res) => {
   res.send(`
@@ -28,7 +49,7 @@ app.post('/create_invoice', async (req, res) => {
     //const { total_amount, description, store_name } = req.body;
 
     const response = await axios.post('https://app.paydunya.com/api/v1/checkout-invoice/create', {
-      "invoice": {"total_amount": 5000, "description": "Chaussure VANS dernier modèle"},"store": {"name": "Magasin le Choco"}
+      "invoice": { "total_amount": 5000, "description": "Chaussure VANS dernier modèle" }, "store": { "name": "Magasin le Choco" }
     }, {
       headers: {
         'Content-Type': 'application/json',
@@ -55,30 +76,40 @@ app.post('/create_invoice', async (req, res) => {
 });
 
 //push api
-app.post('/get_disbursement_invoice', async (req, res) => {
+app.get('/get_disbursement_invoice', async (req, res) => {
   try {
-      console.log('invoice details:---->', req.body)
-      const response = await axios.post('https://app.paydunya.com/api/v1/disburse/get-invoice', {
-          account_alias: "771111111",
-          amount:"1000",
-          withdraw_mode:"moov-ci"
-      }, {
-          headers: {
-              'Content-Type': 'application/json',
-              'PAYDUNYA-MASTER-KEY': masterKey,
-              'PAYDUNYA-PRIVATE-KEY': privateToken,
-              'PAYDUNYA-TOKEN': paydunyaToken
-          }
-      });
-
-      res.json(response.data);
+    console.log('invoice details:---->', req.body)
+    const response = await axios.post('https://app.paydunya.com/api/v1/disburse/get-invoice', {
+      account_alias: "771111111",
+      amount: "1000",
+      withdraw_mode: "orange-money-senegal"
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'PAYDUNYA-MASTER-KEY': masterKey,
+        'PAYDUNYA-PRIVATE-KEY': privateToken,
+        'PAYDUNYA-TOKEN': paydunyaToken
+      }
+    });
+    if(response.data.response_code==='4002'){
+      console.log('Error:---->', response.data)
+    }
+    res.json(response.data);
   } catch (error) {
-      console.error('Error:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
+    console.error('Error:', error);
+    res.status(500).json({ error: error.message });
   }
 });
 
-const port = 3000;
-app.listen(port, '192.168.195.88', () => {
-  console.log(`Server is running on http://192.168.195.88:${port}`);
+
+app.get('/', (req, res) => {
+  console.log('Detected language:', req.language); // Check detected language
+  res.send(req.t('welcome'));
+});
+
+
+
+const port = 12000;
+app.listen(port, '192.168.10.18', () => {
+  console.log(`Server is running on http://192.168.10.18:${port}`);
 });
