05a1dab620ae22c80b711b63db6e51a794b15c59
alshakibeelahi <alshakibeelahi@gmail.com>
2023-11-13 09:40:00 +0600

    key changes

--- index.js
+++ index.js
@@ -6,29 +6,29 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //setting up i18next
-const i18next = require('i18next');
-const i18nextMiddleware = require('i18next-http-middleware');
-const Backend = require('i18next-node-fs-backend');
+// const i18next = require('i18next');
+// const i18nextMiddleware = require('i18next-http-middleware');
+// const Backend = require('i18next-node-fs-backend');
 
-i18next
-.use(Backend)
-.use(i18nextMiddleware.LanguageDetector)
-.init({
-  backend: {
-    loadPath: __dirname + '/locales/{{lng}}/translation.json',
-  },
-  detection: {
-    order: ['header'],
-    caches: ['cookie']
-  },
-  preload: ['en', 'fr'],
-  fallbackLng: 'fr',
-});
-app.use(i18nextMiddleware.handle(i18next));
+// i18next
+// .use(Backend)
+// .use(i18nextMiddleware.LanguageDetector)
+// .init({
+//   backend: {
+//     loadPath: __dirname + '/locales/{{lng}}/translation.json',
+//   },
+//   detection: {
+//     order: ['header'],
+//     caches: ['cookie']
+//   },
+//   preload: ['en', 'fr'],
+//   fallbackLng: 'fr',
+// });
+// app.use(i18nextMiddleware.handle(i18next));
 
-const masterKey = 'i4y97T3G-9Hpt-jGIZ-3XqC-eFjwMSgwYun8';
-const privateToken = 'live_private_usA0HXYhGXCQEOp1JJcAw7qOwoM';
-const paydunyaToken = 'ZouqPb1pKM85ni1WhJF2';
+const masterKey = 'xxxx-xxxx-xxxx-xxxx-xxxx-xxxx-xxxx-xxxx';
+const privateToken = 'xxxx-xxxx-xxxx-xxxx-xxxx-xxxx-xxxx-xxxx';
+const paydunyaToken = 'xxxx-xxxx-xxxx-xxxx-xxxx-xxxx-xxxx-xxxx';
 
 app.get('/disburse-', (req, res) => {
   res.send(`
@@ -44,12 +44,12 @@ app.get('/disburse-', (req, res) => {
   `);
 });
 
-app.post('/create_invoice', async (req, res) => {
+app.get('/create_invoice', async (req, res) => {
   try {
     //const { total_amount, description, store_name } = req.body;
 
     const response = await axios.post('https://app.paydunya.com/api/v1/checkout-invoice/create', {
-      "invoice": { "total_amount": 5000, "description": "Chaussure VANS dernier modèle" }, "store": { "name": "Magasin le Choco" }
+      "invoice": { "total_amount": 1000, "description": "a residence booking payment" }, "store": { "name": "resid plus test" }
     }, {
       headers: {
         'Content-Type': 'application/json',
@@ -80,9 +80,9 @@ app.get('/get_disbursement_invoice', async (req, res) => {
   try {
     console.log('invoice details:---->', req.body)
     const response = await axios.post('https://app.paydunya.com/api/v1/disburse/get-invoice', {
-      account_alias: "771111111",
-      amount: "1000",
-      withdraw_mode: "orange-money-senegal"
+      account_alias: "0757063629",
+      amount: "200",
+      withdraw_mode: "orange-money-ci"
     }, {
       headers: {
         'Content-Type': 'application/json',
